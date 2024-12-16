afd88a56a599543b17334d23d21fb7fd49fc33cc
SeonghaeJo <jsh990324@snu.ac.kr>
2024-12-17 03:18:59 +0900

    [In Progress] Use 4 Nodes (MPI Scatter/Gather)

--- src/model.cu
+++ src/model.cu
@@ -6,9 +6,8 @@
 #include "layer.h"
 #include "model.h"
 
-// #define DEBUG 1
-
 #define NUM_GPUS 4
+#define NUM_NODES 4
 
 /* [Model Parameters]
  * _w: Weight parameter
@@ -30,13 +29,6 @@ Parameter *linear3_w[NUM_GPUS], *linear3_b[NUM_GPUS];
 
 void alloc_and_set_parameters(float *param, size_t param_size) {
   size_t pos[4] = {0};
-
-  int mpi_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-
-  #ifdef DEBUG
-  if (mpi_rank == 0) printf("alloc_and_set_parameters\n");
-  #endif
 
   #pragma omp parallel for num_threads(NUM_GPUS)
   for (int g = 0; g < NUM_GPUS; g++) {
@@ -134,6 +126,9 @@ Activation *conv3_a[NUM_GPUS], *pool3_a[NUM_GPUS];
 Activation *concat_a[NUM_GPUS];
 Activation *linear0_a[NUM_GPUS], *linear1_a[NUM_GPUS], *linear2_a[NUM_GPUS], *linear3_a[NUM_GPUS];
 
+int *inputs_mpi;
+float *outputs_mpi;
+
 int *inputs_d[NUM_GPUS];
 
 void alloc_activations() {
@@ -141,29 +136,35 @@ void alloc_activations() {
   for (int g = 0; g < NUM_GPUS; g++) {
     CHECK_CUDA(cudaSetDevice(g));
 
-    emb_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, SEQ_LEN, 4096});
-    permute_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 4096, SEQ_LEN});
-    conv0_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024, SEQ_LEN - 2});
-    pool0_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024});
-    conv1_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024, SEQ_LEN - 4});
-    pool1_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024});
-    conv2_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024, SEQ_LEN - 6});
-    pool2_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024});
-    conv3_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024, SEQ_LEN - 8});
-    pool3_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024});
-    concat_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 4096});
-    linear0_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 2048});
-    linear1_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 1024});
-    linear2_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 512});
-    linear3_a[g] = new Activation({NUM_SENTENCES / NUM_GPUS, 2});
+    emb_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, SEQ_LEN, 4096});
+    permute_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096, SEQ_LEN});
+    conv0_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024, SEQ_LEN - 2});
+    pool0_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024});
+    conv1_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024, SEQ_LEN - 4});
+    pool1_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024});
+    conv2_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024, SEQ_LEN - 6});
+    pool2_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024});
+    conv3_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024, SEQ_LEN - 8});
+    pool3_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024});
+    concat_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096});
+    linear0_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 2048});
+    linear1_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 1024});
+    linear2_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 512});
+    linear3_a[g] = new Activation({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 2});
 
-    conv0_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_GPUS, 4096 * 3, 16});
-    conv1_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_GPUS, 4096 * 5, 16});
-    conv2_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_GPUS, 4096 * 7, 16});
-    conv3_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_GPUS, 4096 * 9, 16});
+    conv0_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096 * 3, 16});
+    conv1_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096 * 5, 16});
+    conv2_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096 * 7, 16});
+    conv3_in_spread[g] = new HalfTensor({NUM_SENTENCES / NUM_NODES / NUM_GPUS, 4096 * 9, 16});
 
-    CHECK_CUDA(cudaMalloc(&inputs_d[g], NUM_SENTENCES / NUM_GPUS * SEQ_LEN * sizeof(int)));
+    CHECK_CUDA(cudaMalloc(&inputs_d[g], NUM_SENTENCES / NUM_NODES / NUM_GPUS * SEQ_LEN * sizeof(int)));
   }
+
+  CHECK_CUDA(cudaMallocHost((void **)&inputs_mpi, NUM_SENTENCES * SEQ_LEN * sizeof(int)));
+  memset(inputs_mpi, 0, NUM_SENTENCES * SEQ_LEN * sizeof(int));
+
+  CHECK_CUDA(cudaMallocHost((void **)&outputs_mpi, NUM_SENTENCES * 2 * sizeof(float)));
+  memset(outputs_mpi, 0, NUM_SENTENCES * 2 * sizeof(float));
 }
 
 void free_activations() {
@@ -194,6 +195,9 @@ void free_activations() {
 
     CHECK_CUDA(cudaFree(inputs_d[g]));
   }
+
+  CHECK_CUDA(cudaFreeHost(inputs_mpi));
+  CHECK_CUDA(cudaFreeHost(outputs_mpi));
 }
 
 /* [Model Computation: Sentiment Analysis Task] */
@@ -206,60 +210,75 @@ void predict_sentiment(int *inputs, float *outputs, size_t n_samples) {
 
   int mpi_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-  if (mpi_rank == 0) {
 
-    #pragma omp parallel for num_threads(NUM_GPUS)
-    for (int g = 0; g < NUM_GPUS; g++) {
-
-      CHECK_CUDA(cudaSetDevice(g));
-
-      // inputs = [num_sentences * SEQ_LEN]
-      CHECK_CUDA(cudaMemcpy(inputs_d[g], &inputs[(NUM_SENTENCES / NUM_GPUS) * SEQ_LEN * g], 
-                            NUM_SENTENCES / NUM_GPUS * SEQ_LEN * sizeof(int), 
-                            cudaMemcpyHostToDevice));
-
-      Embedding(inputs_d[g], emb_w[g], emb_a[g]);
-
-      Permute(emb_a[g], permute_a[g]);
-
-      Conv1D(permute_a[g], conv0_w[g], conv0_b[g], conv0_a[g], conv0_in_spread[g]);
-
-      ReLU(conv0_a[g]);
-      GetMax(conv0_a[g], pool0_a[g]);
-
-      Conv1D(permute_a[g], conv1_w[g], conv1_b[g], conv1_a[g], conv1_in_spread[g]);
-      ReLU(conv1_a[g]);
-      GetMax(conv1_a[g], pool1_a[g]);
-
-      Conv1D(permute_a[g], conv2_w[g], conv2_b[g], conv2_a[g], conv2_in_spread[g]);
-      ReLU(conv2_a[g]);
-      GetMax(conv2_a[g], pool2_a[g]);
-
-      Conv1D(permute_a[g], conv3_w[g], conv3_b[g], conv3_a[g], conv3_in_spread[g]);
-      ReLU(conv3_a[g]);
-      GetMax(conv3_a[g], pool3_a[g]);
-
-      Concat(pool0_a[g], pool1_a[g], pool2_a[g], pool3_a[g], concat_a[g]);
-
-      if (n_samples == 1) {
-        continue;
-      }
-
-      Linear(concat_a[g], linear0_w[g], linear0_b[g], linear0_a[g]);
-      ReLU(linear0_a[g]);
-
-      Linear(linear0_a[g], linear1_w[g], linear1_b[g], linear1_a[g]);
-      ReLU(linear1_a[g]);
-
-      Linear(linear1_a[g], linear2_w[g], linear2_b[g], linear2_a[g]);
-      ReLU(linear2_a[g]);
-
-      Linear_narrow(linear2_a[g], linear3_w[g], linear3_b[g], linear3_a[g]);
-
-      // outputs = [num_sentences * N_CLASSES]
-      CHECK_CUDA(cudaMemcpy(&outputs[(NUM_SENTENCES / NUM_GPUS) * 2 * g], linear3_a[g]->buf,
-                            NUM_SENTENCES / NUM_GPUS * 2 * sizeof(float),
-                            cudaMemcpyDeviceToHost));
-    }
+  if (mpi_rank != 0) {
+    inputs = inputs_mpi;
+    outputs = outputs_mpi;
   }
+
+  int mpi_in_count = (NUM_SENTENCES / NUM_NODES) * SEQ_LEN;
+  MPI_Scatter((void *)inputs, mpi_in_count, MPI_INT, 
+              (void *)(&inputs[mpi_in_count * mpi_rank]), mpi_in_count, MPI_INT,
+              0, MPI_COMM_WORLD);
+
+  #pragma omp parallel for num_threads(NUM_GPUS)
+  for (int g = 0; g < NUM_GPUS; g++) {
+
+    CHECK_CUDA(cudaSetDevice(g));
+
+    // inputs = [num_sentences * SEQ_LEN]
+    int input_start = (NUM_SENTENCES / NUM_NODES / NUM_GPUS) * SEQ_LEN * (NUM_GPUS * mpi_rank + g);
+    CHECK_CUDA(cudaMemcpy(inputs_d[g], &inputs[input_start], 
+                          NUM_SENTENCES / NUM_NODES / NUM_GPUS * SEQ_LEN * sizeof(int), 
+                          cudaMemcpyHostToDevice));
+
+    Embedding(inputs_d[g], emb_w[g], emb_a[g]);
+
+    Permute(emb_a[g], permute_a[g]);
+
+    Conv1D(permute_a[g], conv0_w[g], conv0_b[g], conv0_a[g], conv0_in_spread[g]);
+
+    ReLU(conv0_a[g]);
+    GetMax(conv0_a[g], pool0_a[g]);
+
+    Conv1D(permute_a[g], conv1_w[g], conv1_b[g], conv1_a[g], conv1_in_spread[g]);
+    ReLU(conv1_a[g]);
+    GetMax(conv1_a[g], pool1_a[g]);
+
+    Conv1D(permute_a[g], conv2_w[g], conv2_b[g], conv2_a[g], conv2_in_spread[g]);
+    ReLU(conv2_a[g]);
+    GetMax(conv2_a[g], pool2_a[g]);
+
+    Conv1D(permute_a[g], conv3_w[g], conv3_b[g], conv3_a[g], conv3_in_spread[g]);
+    ReLU(conv3_a[g]);
+    GetMax(conv3_a[g], pool3_a[g]);
+
+    Concat(pool0_a[g], pool1_a[g], pool2_a[g], pool3_a[g], concat_a[g]);
+
+    if (n_samples == 1) {
+      continue;
+    }
+
+    Linear(concat_a[g], linear0_w[g], linear0_b[g], linear0_a[g]);
+    ReLU(linear0_a[g]);
+
+    Linear(linear0_a[g], linear1_w[g], linear1_b[g], linear1_a[g]);
+    ReLU(linear1_a[g]);
+
+    Linear(linear1_a[g], linear2_w[g], linear2_b[g], linear2_a[g]);
+    ReLU(linear2_a[g]);
+
+    Linear_narrow(linear2_a[g], linear3_w[g], linear3_b[g], linear3_a[g]);
+
+    int output_start = (NUM_SENTENCES / NUM_NODES / NUM_GPUS) * 2 * (NUM_GPUS * mpi_rank + g);
+    // outputs = [num_sentences * N_CLASSES]
+    CHECK_CUDA(cudaMemcpy(&outputs[output_start], linear3_a[g]->buf,
+                          (NUM_SENTENCES / NUM_NODES / NUM_GPUS) * 2 * sizeof(float),
+                          cudaMemcpyDeviceToHost));
+  }
+
+  int mpi_out_count = (NUM_SENTENCES / NUM_NODES) * 2;
+  MPI_Gather((void *)(&outputs[mpi_out_count * mpi_rank]), mpi_out_count, MPI_FLOAT,
+              (void *)(&outputs[mpi_out_count * mpi_rank]), mpi_out_count, MPI_FLOAT, 
+              0, MPI_COMM_WORLD);
 }
